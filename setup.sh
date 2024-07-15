9569fae02d32816c04aabdc3134a42ba7e3296ef
Ben Ohling <b.ohling@outlook.com>
2024-07-15 13:21:12 -0700

    fix: overwrite with cp and force reinstallation of clipp

--- setup.sh
+++ setup.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SRC_DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
 python3 -m venv /usr/local/share/howlong
 source /usr/local/share/howlong/bin/activate
-python -m pip install clipp
+python -m pip install --upgrade --force-reinstall clipp
 deactivate
 
-cp ./howlong /usr/local/bin
+yes | /bin/cp -rf "$SRC_DIR"/howlong /usr/local/bin
